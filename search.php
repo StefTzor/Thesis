331ea60a34daaab994e0eddfd096759f1f74c381
Stefanos Tzortzoglou <steve.tzortz@hotmail.com>
2019-10-21 22:29:20 +0300

    search.php 0.1

---
+++ search.php
@@ -0,0 +1,43 @@
+<?php
+require_once('load.php');
+
+// Handle logins
+if($_SERVER["REQUEST_METHOD"] == "POST") {
+    $login_status = $login->verify_login($_POST);
+}
+    
+// Verify session
+if ( $login->verify_session() ) {
+    $user = $login->user;
+    $userType = $user->userType;
+    
+    include('header.php'); ?>
+
+    <div class="wrapper">
+        <?php
+            $subjects = $db->query("SELECT * FROM subjects");
+            echo "<table><tbody><tr><th>Εξάμηνο</th><th>Μάθημα</th></tr>\r\n";
+            foreach ($subjects as $result) {
+                echo "<tr data-href='$result->id.php?subject=$result->name'>\r\n";
+                echo "<td>" . $result->semester . "</td>\r\n";
+                echo "<td>" . $result->name . "</td>\r\n";
+                echo "</tr>\r\n";
+                ?>
+                <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
+                <script>
+                    $(document).ready(function(){
+                        $('table tr').click(function(){
+                            window.location = $(this).data('href');
+                            return false;
+                        });
+                    });
+                </script>
+                <?php
+            }
+            echo "</tbody></table>\r\n";
+            ?>
+    </div>
+    <?php include('footer.php');
+} else {
+    include( 'login.php' );
+}
