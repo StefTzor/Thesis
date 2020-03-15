200291bcd891f4621e8c9f39fafdc516fa44474e
Stefanos Tzortzoglou <steve.tzortz@hotmail.com>
2020-03-15 17:02:43 +0200

    fix: footer.date

--- footer.php
+++ footer.php
@@ -1,5 +1,5 @@
     <center>
-    &copy; 2019 Πανεπιστήμιο Δυτικής Μακεδονίας &bull; Τμήμα Πληροφορικής &bull; Τζώρτζογλου Στέφανος
+    &copy; 2019-<?php echo date("Y"); ?> Πανεπιστήμιο Δυτικής Μακεδονίας &bull; Τμήμα Πληροφορικής &bull; Τζώρτζογλου Στέφανος
     <br/>
     <span><img src="/grapta/includes/uowm.png" height=42></span>
     </center>
